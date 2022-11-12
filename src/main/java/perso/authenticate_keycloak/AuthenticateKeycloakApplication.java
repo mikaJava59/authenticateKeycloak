ecd6934f1ef8318171954a751e0ae7706b4fadef
michael <speedsub@gmail.com>
2022-11-12 16:38:02 +0100

    first commit

---
+++ src/main/java/perso/authenticate_keycloak/AuthenticateKeycloakApplication.java
@@ -0,0 +1,13 @@
+package perso.authenticate_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticateKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticateKeycloakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/perso/authenticate_keycloak/AuthenticateKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package perso.authenticate_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticateKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
